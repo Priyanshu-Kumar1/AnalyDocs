f090db6b062d76bc2b9ef5b37e97f9d1f4e9de15
Priyanshu Kumar <rprem058@gmail.com>
2025-07-20 12:34:19 +0530

    some modifications

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
-# Create your models here.
+class User(AbstractUser):
+    """
+    Custom User model that extends the default Django User model.
+    This allows for future extensibility if needed.
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+     
+    def __str__(self):
+        return self.username
+    
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        db_table = 'auth_user'
+    
